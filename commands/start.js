ca6879cae6a8a67a0952ade35d83ad0174c3119f
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-06-05 10:26:34 +0800

    changes

--- commands/start.js
+++ commands/start.js
@@ -6,9 +6,9 @@ module.exports.run = async(client, msg, args) => {
     hooman = msg.author.id
     const item = games[Math.floor(Math.random() * games.length)]
     msg.channel.send(item.games)
-    msg.channel.send(`\n\n**Good luck, you have 5 tries (This counts every message that you send from now until you get the answer right or you run out of tries.)**`)
+    msg.channel.send(`\n\n**Good luck, you have 3 tries (This counts every message that you send from now until you get the answer right or you run out of tries.)**`)
     const collector = msg.channel.createMessageCollector(
-        m => m.author.id == hooman, {max: 5, time: 1200000}
+        m => m.author.id == hooman, {max: 3}
     );
     collector.on('collect', m => {
         if (m.content.toLowerCase() == item.answers) {
@@ -18,11 +18,16 @@ module.exports.run = async(client, msg, args) => {
             collector.stop(item.answers)
         }
     });
-    collector.on('end', collected => {
-        if (collected.size == 5) {
-            msg.channel.send(`You did not get it! That's 5 tries. You opened an ugly bathroom.`)
-            console.log(collected)
+    collector.on('end', (collected, reason) => {
+        if (reason && reason === item.answers) {
             return
+        }
+        else {
+            if (collected.size == 5) {
+                msg.channel.send('https://media.discordapp.net/attachments/847825770382229514/850426337154826270/old-dirty-toilet-picture-id523086917.png')
+                return
+            }
+            
         }
     });
 }
