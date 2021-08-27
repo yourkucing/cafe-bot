d555d74ee28ebd2ecab499fbe75e5ba2a57eea6d
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-08-27 14:47:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -52,18 +52,33 @@ client.on('ready', () => {
     if (message === "intas" || message === "intas!" || message === "intas." || message === "*intas*" || message === "**intas**" || message === "\"intas\"") {
         console.log("she said it!")
         player = msg.guild.members.cache.get(msg.author.id)
-        for (i in rooms) {
-            console.log(rooms[i].room1)
-            if (player.roles.cache.get(rooms[i].room1)) {
-                try {
-                    player.roles.remove(msg.guild.roles.cache.get(rooms[i].room1))
-                    player.roles.add(msg.guild.roles.cache.get(rooms[i].room2))
-                    return
-                }
-                catch(e) {
-                    console.log(e)
-                }
-            }
+        if (player.roles.cache.get("880679525669806122")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679525669806122"))
+            player.roles.add(msg.guild.roles.cache.get("880679579616948285"))
+        }
+        else if (player.roles.cache.get("880679579616948285")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679579616948285"))
+            player.roles.add(msg.guild.roles.cache.get("880679639222190142"))
+        }
+        else if (player.roles.cache.get("880679598747176960")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679598747176960"))
+            player.roles.add(msg.guild.roles.cache.get("880679675146424340"))
+        }
+        else if (player.roles.cache.get("880679621736153118")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679621736153118"))
+            player.roles.add(msg.guild.roles.cache.get("880679525669806122"))
+        }
+        else if (player.roles.cache.get("880679639222190142")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679639222190142"))
+            player.roles.add(msg.guild.roles.cache.get("880679598747176960"))
+        }
+        else if (player.roles.cache.get("880679655961665567")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679655961665567"))
+            player.roles.add(msg.guild.roles.cache.get("880679621736153118"))
+        }
+        else if (player.roles.cache.get("880679675146424340")) {
+            player.roles.remove(msg.guild.roles.cache.get("880679675146424340"))
+            player.roles.add(msg.guild.roles.cache.get("880679655961665567"))
         }
     }
     else if (message == "vardis" || message == "vardis!" || message == "vardis." || message == "*vardis*" || message == "**vardis**" || message == "\"vardis\"") {
