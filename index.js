fda1286d38983e2af7b69c689c799f6afd6e4c85
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-11-27 15:56:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({disableEveryone: false});
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = "cafe ";
 const fs = require('fs').promises;
 const path = require('path');
