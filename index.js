576bcd8e86c4e026e8902c91d7a3c868f078c18b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-02-25 16:42:16 +0800

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS] });
 const prefix = "cafe ";
 const fs = require('fs').promises;
 const path = require('path');
