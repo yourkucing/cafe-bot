6b6b8f8b5ecfe951f8cc806cbeb358a12e5db9bc
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-05-30 19:57:12 +0200

    MessageEmbed was not defined

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS], disableEveryone: false, partials: ['MESSAGE', 'REACTION']});
 const prefix = "cafe ";
 const fs = require('fs').promises;
