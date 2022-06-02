871ab365e9fc26d41e4d093425345287a43d182b
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-06-02 08:43:45 +0200

    Update play.js

--- commands/game/play.js
+++ commands/game/play.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
-
-    client.channels.cache.get(args).messages.fetch({limit: 99}).then(m => {
+    client.channels.cache.get(args[0]).messages.fetch({limit: 99}).then(m => {
         nummessages = m.size - 1
-        client.channels.cache.get(args).messages.fetch({limit: nummessages}).then(k => {
-            msg.guild.channels.cache.get(args).bulkDelete(k)
+        client.channels.cache.get(args[0]).messages.fetch({limit: nummessages}).then(k => {
+            msg.guild.channels.cache.get(args[0]).bulkDelete(k)
         })
     })    
 }
